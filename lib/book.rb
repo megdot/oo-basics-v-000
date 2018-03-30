451a89d347f4b2bdae742956506e89840a80e1e3
Megan Dotto <megan.dotto@gmail.com>
2018-03-30 21:35:14 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,5 +7,20 @@ class Book
     @title
   end
 
+def author=(author)
+  @author = author
+end
+
+def author
+  @author
+end
+
+def page_count=(num)
+  @page_count = num
+end
+
+def page_count
+  @page_count
+end
 
 end
